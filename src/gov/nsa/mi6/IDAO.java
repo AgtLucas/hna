cb981b80837a8c4fc045c021308fcb8ea646a736
Lucas Churchill <lucas@agtlucas.com>
2014-03-16 20:52:56 -0300

    AbstractDAO e IDAO

--- src/gov/nsa/mi6/IDAO.java
+++ src/gov/nsa/mi6/IDAO.java
@@ -6,10 +6,32 @@
 
 package gov.nsa.mi6;
 
+import java.util.List;
+
 /**
  *
  * @author AgtLucas
  */
 public interface IDAO {
+    
+    Long countAll() throws Exception;
+
+    Object create(Object o) throws Exception;
+
+    void delete(Object o) throws Exception;
+
+    List findAll() throws Exception;
+
+    Object findById(Integer theId) throws Exception;
+
+    Object findByName(String theName) throws Exception;
+
+    Object getNewInstance();
+
+    List list(Integer firstResult, Integer maxResults) throws Exception;
+
+    void removeAll() throws Exception;
+
+    void save(Object o) throws Exception; 
     
 }
